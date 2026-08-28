53199afba7b21a187b1dc206ff9b66b8377908fd
Bayram Barış Gök <138613519+Bbar1sgok@users.noreply.github.com>
2026-08-28 15:10:44 +0300

    Update MajorityElement.py
    new solution

--- Easy/MajorityElement.py
+++ Easy/MajorityElement.py
@@ -9,12 +9,18 @@
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        candidate = None
+        result = 0
         count = 0
 
         for num in nums:
+            # If count drops to 0, pick the current number as the new candidate
             if count == 0:
-                candidate = num
-            count += 1 if num == candidate else -1
+                result = num
 
-        return candidate
+            # Increment if match, otherwise decrement
+            if num == result:
+                count += 1
+            else:
+                count -= 1
+
+        return result
